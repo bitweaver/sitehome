a9626c2d21fe9d317c6562fe9bf7461f9b16626f
Nick Palmer <nick@sluggardy.net>
2008-11-10 16:44:16 +0000

    Fix verify permission call so it is the right call in the right place.

--- edit.php
+++ edit.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_sitehome/edit.php,v 1.4 2008/10/20 21:40:11 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_sitehome/edit.php,v 1.5 2008/11/10 16:44:16 nickpalmer Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: edit.php,v 1.4 2008/10/20 21:40:11 spiderr Exp $
+ * $Id: edit.php,v 1.5 2008/11/10 16:44:16 nickpalmer Exp $
  * @package sitehome
  * @subpackage functions
  */
@@ -21,10 +21,10 @@ require_once( '../bit_setup_inc.php' );
 // Is package installed and enabled
 $gBitSystem->verifyPackage( 'sitehome' );
 
-// Now check permissions to access this page
-$gContent->verifyEditPermission();
-
 require_once(SITEHOME_PKG_PATH.'lookup_sitehome_inc.php' );
+
+// Now check permissions to access this page
+$gContent->verifyUpdatePermission();
 
 if( isset( $_REQUEST['sitehome']["title"] ) ) {
 	$gContent->mInfo["title"] = $_REQUEST['sitehome']["title"];
