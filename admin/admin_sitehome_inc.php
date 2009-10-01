fa96e1838bbdf80ed087ce8a616905a5533c5054
wjames5 <will@tekimaki.com>
2009-10-01 14:17:05 +0000

    remove ref to non-existant copyright.txt file

--- admin/admin_sitehome_inc.php
+++ admin/admin_sitehome_inc.php
@@ -1,7 +1,7 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_sitehome/admin/admin_sitehome_inc.php,v 1.4 2009/10/01 13:45:48 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_sitehome/admin/admin_sitehome_inc.php,v 1.5 2009/10/01 14:17:05 wjames5 Exp $
 // Copyright (c) 2005 bitweaver SiteHome
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 require_once( SITEHOME_PKG_PATH.'BitSiteHome.php' );
